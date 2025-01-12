3643efcfaaf083c3fbde2d9673ca4fb032c8cac5
Allovallo <grossco@meta.ua>
2025-01-12 18:41:24 +0200

    files updated

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const books = require("./books");
+const { program } = require("commander");
 
-console.log(books);
+const books = require("./books");
 
 const invokeAction = async ({ action, id, title, author }) => {
   switch (action) {
@@ -13,9 +13,41 @@ const invokeAction = async ({ action, id, title, author }) => {
     case "add":
       const newBook = await books.add({ title, author });
       return console.log(newBook);
+    case "updateById":
+      const updateBook = await books.updateById(id, { title, author });
+      return console.log(updateBook);
+    case "deleteById":
+      const deleteBook = await books.deleteById(id);
+      return console.log(deleteBook);
+    default:
+      console.log("UNKNOWN ACTION!");
   }
 };
 
 // invokeAction({ action: "read" });
 // invokeAction({ action: "getById", id: "CTHE0f1kkWwqS5sL2tI8_" });
-invokeAction({ action: "add", title: "X", author: "Y" });
+// invokeAction({ action: "add", title: "X", author: "Y" });
+// invokeAction({
+//   action: "updateById",
+//   id: "WV2a_0a-jCZR6HneYd0Vt",
+//   title: "XXX",
+//   author: "YYY",
+// });
+// invokeAction({ action: "deleteById", id: "WV2a_0a-jCZR6HneYd0Vt" });
+
+// const actionIndex = process.argv.indexOf("--action");
+// if (actionIndex !== -1) {
+//   const action = process.argv[actionIndex + 1];
+//   invokeAction({ action });
+// }
+
+program
+  .option("-a, --action, <type>")
+  .option("-i, --id, <type>")
+  .option("-t, --title, <type>")
+  .option("-u, --author, <type>");
+
+program.parse();
+
+const options = program.opts();
+console.log(options);
--- books/index.js
+++ books/index.js
@@ -1,6 +1,7 @@
 const fs = require("fs/promises");
-const { nanoid } = require("nanoid");
 const path = require("path");
+const { nanoid } = require("nanoid");
+const { isNull } = require("util");
 
 const booksPath = path.join(__dirname, "books.json");
 
@@ -23,12 +24,36 @@ const add = async (data) => {
     ...data,
   };
   books.push(newBook);
-  await fs.writeFile(booksPath, JSON.stringify(books));
+  await fs.writeFile(booksPath, JSON.stringify(books, null, 2));
   return newBook;
+};
+
+const updateById = async (id, data) => {
+  const books = await getAll();
+  const index = books.findIndex((item) => item.id === id);
+  if (index === -1) {
+    return null;
+  }
+  books[index] = { id, ...data };
+  await fs.writeFile(booksPath, JSON.stringify(books, null, 2));
+  return books[index];
+};
+
+const deleteById = async (id) => {
+  const books = await getAll();
+  const index = books.findIndex((item) => item.id === id);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = books.splice(index, 1);
+  await fs.writeFile(booksPath, JSON.stringify(books, null, 2));
+  return result;
 };
 
 module.exports = {
   getAll,
   getById,
   add,
+  updateById,
+  deleteById,
 };
