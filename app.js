ee4955e96909863da67763f2e71ff0facd15f0e1
Allovallo <grossco@meta.ua>
2025-01-12 16:09:28 +0200

    files updated

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
 const books = require("./books");
 
 console.log(books);
+
+const invokeAction = async ({ action, id, title, author }) => {
+  switch (action) {
+    case "read":
+      const allBooks = await books.getAll();
+      return console.log(allBooks);
+    case "getById":
+      const oneBook = await books.getById(id);
+      return console.log(oneBook);
+    case "add":
+      const newBook = await books.add({ title, author });
+      return console.log(newBook);
+  }
+};
+
+// invokeAction({ action: "read" });
+// invokeAction({ action: "getById", id: "CTHE0f1kkWwqS5sL2tI8_" });
+invokeAction({ action: "add", title: "X", author: "Y" });
--- books/index.js
+++ books/index.js
@@ -1,5 +1,34 @@
-const getAll = () => {};
+const fs = require("fs/promises");
+const { nanoid } = require("nanoid");
+const path = require("path");
+
+const booksPath = path.join(__dirname, "books.json");
+
+const getAll = async () => {
+  const data = await fs.readFile(booksPath);
+  return JSON.parse(data);
+};
+
+const getById = async (id) => {
+  const books = await getAll();
+  const result = books.find((item) => item.id === id);
+  return result || null;
+};
+
+const add = async (data) => {
+  const books = await getAll();
+
+  const newBook = {
+    id: nanoid(),
+    ...data,
+  };
+  books.push(newBook);
+  await fs.writeFile(booksPath, JSON.stringify(books));
+  return newBook;
+};
 
 module.exports = {
   getAll,
+  getById,
+  add,
 };
