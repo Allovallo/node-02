177caf50a61c304a0df83a2fef78d525ac7e3f20
Allovallo <grossco@meta.ua>
2025-01-18 18:02:53 +0200

    files updated

--- project-02/app.js
+++ project-02/app.js
@@ -8,6 +8,15 @@ const invokeAction = async ({ action, id, title, author }) => {
     case "getById":
       const oneBook = await books.getById(id);
       return console.log(oneBook);
+    case "add":
+      const newBook = await books.add({ title, author });
+      return console.log(newBook);
+    case "updateById":
+      const updateBook = await books.updateById(id, { title, author });
+      return console.log(updateBook);
+    case "deleteById":
+      const deleteBook = await books.deleteById(id);
+      return console.log(deleteBook);
 
     default:
       return console.log("Unknown action :(");
@@ -15,4 +24,12 @@ const invokeAction = async ({ action, id, title, author }) => {
 };
 
 // invokeAction({ action: "read" });
-invokeAction({ action: "getById", id: "YxhM4QDxPeA3SmPHcEZPJ" });
+// invokeAction({ action: "getById", id: "YxhM4QDxPeA3SmPHcEZPJ" });
+// invokeAction({ action: "add", title: "X", author: "Y" });
+// invokeAction({
+//   action: "updateById",
+//   id: "S0dAKFY_px1-55wheEX13",
+//   title: "XXX",
+//   author: "YYY",
+// });
+invokeAction({ action: "deleteById", id: "S0dAKFY_px1-55wheEX13" });
